01a163e6a169f005519a70236920830283ff86ae
Raman Amatya <ramanamatya@gmail.com>
2019-04-04 21:03:48 +0545

    comments on route.

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from './App';
-import NameLabel from './NameLabel';
+import Comment from './Comment';
 
 export default function () {
   return (
     <Switch>
       <Route path="/" exact component={App} />
-      <Route path="/active1" component={NameLabel} />
+      <Route path="/active1" component={Comment} />
       <Route path="/active2" component={App} />
       <Route path="/active3" component={App} />
     </Switch>
